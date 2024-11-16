1edd9a5767640f0f3be17760b886c1a1a6b3feb7
Gokul_raj_4419 <35720127+google4627@users.noreply.github.com>
2024-11-16 07:57:58 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 IMAGE_NAME="google4627/sample-website"
 IMAGE_TAG="latest"
-echo "Building Docker image..."
+echo "Building Docker image. .."
 docker build -t $IMAGE_NAME:$IMAGE_TAG .
 if [[ $? -eq 0 ]]; then
     echo "Docker image built successfully: $IMAGE_NAME:$IMAGE_TAG"
